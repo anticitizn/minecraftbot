78c497ce65648a1cc29f989a1d148fca315ec992
D. Neykov <metheorita@gmail.com>
2023-07-07 16:37:48 +0200

    Reorder functions in main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import asyncio
 
 import discord
@@ -16,6 +15,19 @@ async def on_ready():
     print(f'Logged in as {client.user}')
     client.loop.create_task(mc_status_loop())
 
+async def mc_status_loop():
+    while True:
+        await get_mc_status()
+        await asyncio.sleep(10)  # Wait for 10 seconds
+
+async def get_mc_status():
+    try:
+        status = server.status()
+        print("{0} players online".format(status.players.online))
+        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="{0} players online".format(status.players.online)))
+    except Exception as e:
+        print("An error occurred: ", e)
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -26,19 +38,6 @@ async def on_message(message):
         if not query.players.names:
             query.players.names.append("No one!")
         await message.channel.send(f"**Players online:** {', '.join(query.players.names)}")
-
-async def get_mc_status():
-    try:
-        status = server.status()
-        print("{0} players online".format(status.players.online))
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="{0} players online".format(status.players.online)))
-    except Exception as e:
-        print("An error occurred: ", e)
-
-async def mc_status_loop():
-    while True:
-        await get_mc_status()
-        await asyncio.sleep(10)  # Wait for 10 seconds
 
 def get_token():
     with open("token.txt", "r") as file:
